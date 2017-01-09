0f57620c627f4fbe94dbd4a3a9a19c35e660a294
Sameer Jathavedan <sameerj137@gmail.com>
2017-01-09 16:36:22 +0000

    Implemented upto search.php

--- public/search.php
+++ public/search.php
@@ -6,9 +6,11 @@
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"], store in $places
+    $places = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name, admin_name1) AGAINST (?)", $_GET["geo"]);
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
     print(json_encode($places, JSON_PRETTY_PRINT));
+    
 
 ?>
