c23e943a83fc8469b86b944ee51805a1e203fdf1
Sameer Jathavedan <sameerj137@gmail.com>
2017-01-09 18:54:38 +0000

    personal touch: heading2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -102,7 +102,7 @@ function addMarker(place)
         var l = data.length;
     
         // list articles in info window
-        var articles = "<center><h1><b>"+place.place_name+"</b></h1></center><br>Number of articles today: "+l+"<br><ul>";
+        var articles = "<center><h3><b>"+place.place_name+place.admin_name1+"</b></h3></center>Number of articles today: "+l+"<br><ul>";
         
             // if there are articles, show title as link
             if (l != 0)
